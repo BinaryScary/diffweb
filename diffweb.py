70f4c2d1db1b81017ca9cd54b5e10bce347d6497
Samwong156 <sam_wong156@hotmail.com>
2022-04-02 14:43:58 -0400

    Added json file support

--- diffweb.py
+++ diffweb.py
@@ -7,6 +7,8 @@ import unicodedata # filename slugify
 import re # filename slugify
 import telegram_send # telegram messages
 import argparse # argument parsing
+import jsonpath_ng # jsonpath
+import traceback # exception tracebacks
 
 # disable ssl warnings
 import urllib3
@@ -34,22 +36,10 @@ def slugify(value, allow_unicode=False):
     value = re.sub(r'[^\w\s-]', '', value.lower())
     return re.sub(r'[-\s]+', '-', value).strip('-_')
 
-def visualize(config):
-    for item in config:
-        if "type" in item and item["type"] == "json":
-            continue
-
-        resp = requests.get(item["url"], headers=headers, verify=False)
-        resp_text = parse_html(resp.text, item)
-
-        print(item["name"])
-        print("--------")
-        print("\n".join(resp_text))
-        print("--------\n")
-
-def parse_html(html, config_item):
+# html type items
+def parse_html(html_text, config_item):
     # format html
-    beaut = BeautifulSoup(html, 'html.parser')
+    beaut = BeautifulSoup(html_text, 'html.parser')
 
     # remove elements from html if del-selector provided
     if "del-selector" in config_item:
@@ -64,31 +54,69 @@ def parse_html(html, config_item):
 
     return html_lines
 
-def change_detection(config,diffs_path='./diffs/'):
+# json type items
+def parse_json(json_text, config_item):
+    json_obj = json.loads(json_text)
+
+    # select json-path
+    json_path_obj = jsonpath_ng.parse(config_item["json-path"]).find(json_obj)
+    json_lines = [ match.value for match in json_path_obj ]
+
+    if "json-regex" in config_item:
+        pattern = re.compile(config_item["json-regex"])
+        json_lines = [ line for line in json_lines if pattern.match(line) ]
+
+    return json_lines
+
+
+def change_detection(config,diffs_path='./diffs/',visualize=False):
     for item in config:
-        if "type" in item and item["type"] == "json":
+        # request html from site
+        resp = requests.get(item["url"], headers=headers, verify=False)
+
+        # if status code not 200 send notify and skip
+        if not resp:
+            message = "Item {} HTTP request failed with code: {}".format(item["name"], resp.status_code)
+            print(message)
+            telegram_send.send(messages=[message])
             continue
 
-        # print(item["name"])
-        filename = slugify(item["name"])
-        resp = requests.get(item["url"], headers=headers, verify=False)
-        resp_text = parse_html(resp.text, item)
-
         # if first time running, save html to file
+        filename = slugify(item["name"])
         if not os.path.exists(diffs_path+filename):
             diff_file = open(diffs_path+filename, "w")
             diff_file.write(resp.text)
             diff_file.close()
             continue
+        # read html from file
         diff_file = open(diffs_path+filename, "r")
-        diff_text = parse_html(diff_file, item)
+        diff_text = diff_file.read() 
         diff_file.close
 
+        try:
+            if "type" in item and item["type"] == "json":
+                resp_parsed = parse_json(resp.text, item)
+                diff_parsed = parse_json(diff_text, item)
+            else:
+                resp_parsed = parse_html(resp.text, item)
+                diff_parsed = parse_html(diff_text, item)
+        except Exception as e:
+            message = "Item {} parsing failed with error: {}\n {}".format(item["name"], e, traceback.format_exc())
+            print(message)
+            # telegram_send.send(messages=[message])
+            continue
 
-        # if html is different
-        if diff_text != resp_text:
+        if visualize:
+            print(item["name"])
+            print("--------")
+            print("\n".join(resp_parsed))
+            print("--------\n")
+            continue
+
+        # if list of parsed lines are different
+        if diff_parsed != resp_parsed:
             # create diff
-            compare = difflib.unified_diff(diff_text, resp_text, fromfile='before', tofile='after', n=5)
+            compare = difflib.unified_diff(diff_parsed, resp_parsed, fromfile='before', tofile='after', n=5)
             # send telegram message
             message = "{}\n{}\n---\n{}\n---".format(item["name"],item["url"],"\n".join(compare))
             print(message)
@@ -115,8 +143,4 @@ if __name__ == '__main__':
     if not os.path.exists(diffs_path):
         os.makedirs(diffs_path)
 
-    if args.visualize:
-        visualize(config)
-        quit()
-
-    change_detection(config, diffs_path)
+    change_detection(config, diffs_path, args.visualize)
